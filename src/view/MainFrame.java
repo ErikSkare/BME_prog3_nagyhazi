e66926b1b70285508f2b95fb951459ea0fa1e375
ErikSkare <skareerik55@gmail.com>
2022-11-27 02:10:31 +0100

    no game loaded

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,37 +1,40 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Dimension;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-
 import javax.swing.JFrame;
-
-import logic.Party;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
 	
 	private static final long serialVersionUID = 8456560429229699542L;
+	
+	private PartyView current = null;
+	
+	private CardLayout layout;
+	
+	private JPanel deck;
 
 	public MainFrame() {
 		this.setSize(new Dimension(700, 600));
 		this.setResizable(false);
 		this.setLocationByPlatform(true);
 		
-		Party p = null;
-	      try (FileInputStream fis = new FileInputStream("Test.parti");
-	           ObjectInputStream ois = new ObjectInputStream(fis)) {
-	          p = (Party) ois.readObject();
-	      } catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	      
-	    this.add(new PartyView(this, p));
+		layout = new CardLayout();
+		deck = new JPanel(layout);
+	   
 	    this.setJMenuBar(new MenuBar());
+	    this.add(deck);
+	    
+	    setNoGameState();
+	}
+	
+	private void setNoGameState() {
+		JPanel panel = new JPanel(new BorderLayout());
+		JLabel text = new JLabel("No game loaded!", JLabel.CENTER);
+		panel.add(text, BorderLayout.CENTER);
+		deck.add(panel, "noGame");
 	}
 }
