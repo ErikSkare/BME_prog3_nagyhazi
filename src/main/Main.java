002ce73fab627b27aab6d5d199d9deea630d1fe4
ErikSkare <skareerik55@gmail.com>
2022-11-27 20:34:43 +0100

    main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,14 @@
 package main;
 
+import java.io.File;
+
 import view.MainFrame;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		if(args.length != 1)
+			throw new Exception("Invalid number of parameters. Only provide search directory!");
+		MainFrame.searchDir = new File(args[0]);
 		MainFrame fr = new MainFrame();
 		fr.setVisible(true);
 	}
